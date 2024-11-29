310ead4f2641a339d3c4ea42b3eedc2c98895cbf
Selma Karagøz <selma.krgz@outlook.com>
2024-11-29 16:02:34 +0100

    Add view for create and update post etc

--- Sub2/nissgram2/src/App.tsx
+++ Sub2/nissgram2/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './shared/Layout';
-import HomePage from './pages/HomePage';
-import ProfilePage from './pages/ProfilePage';
-import LoginPage from './pages/auth/LoginPage';
-import RegisterPage from './pages/auth/RegisterPage';
-import NotFoundPage from './pages/NotFoundPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./shared/Layout";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import LoginPage from "./pages/auth/LoginPage";
+import RegisterPage from "./pages/auth/RegisterPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import CreatePost from "./post/createPost"; 
+import UpdatePost from "./post/updatePost";
+
 
 const App: React.FC = () => {
   return (
@@ -17,6 +20,20 @@ const App: React.FC = () => {
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
           <Route path="*" element={<NotFoundPage />} />
+          <Route path="createPost" element={<CreatePost />} /> {/* Riktig bruk av CreatePost */}
+          <Route
+            path="/updatePost"
+            element={
+              <UpdatePost
+                postId={123}
+                existingImgUrl= " "
+                text="This is the current text of the post."
+                onUpdate={(postId, updatedText, updatedImage) => {
+                  console.log("Updated Post:", { postId, updatedText, updatedImage });
+                }}
+              />
+            }
+          />
         </Route>
       </Routes>
     </Router>
---
+++ Sub2/nissgram2/src/types/user.tsx
@@ -0,0 +1,8 @@
+export interface User {
+    id: number;
+    ProfilePicture: string;
+    FirstName: string;
+    LastName: string;
+    About: string;
+  }
+  
